f8bbd9e62bed0bafbf3a8e57b18e97a09d2160eb
plx-edu <plx-edu@hotmail.com>
2022-05-03 22:16:56 +0200

    start of jwt implementation

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from 'src/users/users.module';
 import { AuthService } from './auth.service';
+import { jwtConstants } from './constants';
 import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
-  imports: [UsersModule, PassportModule],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
   providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
---
+++ backend/src/auth/constants.ts
@@ -0,0 +1,8 @@
+export const jwtConstants = {
+  secret: 'secretKey',
+};
+
+/*
+Warning
+Do not expose this key publicly. We have done so here to make it clear what the code is doing, but in a production system you must protect this key using appropriate measures such as a secrets vault, environment variable, or configuration service. 
+*/
