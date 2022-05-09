257f3af86143c35b6872cdb571f7f72b6f6e1154
plx-edu <plx-edu@hotmail.com>
2022-05-09 20:59:01 +0200

    added guard 'extension'

--- backend/src/auth/guards/jwt-auth.guard.ts
+++ backend/src/auth/guards/jwt-auth.guard.ts
@@ -1,5 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Observable } from 'rxjs';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  // Tutorial follow up
+  // Extending guards (1)
+  canActivate(context: ExecutionContext){
+    // Add your custom authentication logic here
+    // for example, call super.logIn(request) to establish a session.
+    console.log(":: canActivate()");
+    return super.canActivate(context);
+  }
+  
+  handleRequest(err, user, info) {
+    // You can throw an exception based on either "info" or "err" arguments
+    console.log(":: handleRequest()");
+    if (err || !user) {
+      throw err || new UnauthorizedException();
+    }
+    return user;
+  }
+}
